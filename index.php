7cf971e9b5b8ba76039a749faf1310c819164ac2
Mason Santora <masonsantora@gmail.com>
2019-03-13 02:44:23 -0400

    Initial Setup
    - Added fontawesome font family
    - Added index landing page
    - Created structure for views/styling
    - Added SASS into styling and compiled css code
    - Added base styling guides

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Mason Santora Portfolio</title>
+        <link rel='stylesheet' type='text/css' href='style/css/style.css'>
+    </head>
+    
+    <body>
+    
+        <h2>test</h2>
+        <i class="fas fa-user-circle"></i>
+        
+    </body>
+
+</html>
