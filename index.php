963e3be78b582bd4ae6e98bfe2f9549c50d19f46
Mason Santora <masonsantora@gmail.com>
2019-03-16 03:41:27 -0400

    Landing Page
    - Updated background image asset name
    - Added base side bar navigation
    - Added base container and text
    - Added javascript plugin that creates/removes text
    - Added helper functions for em/rem conversions
    - Added global styling/settings
    - Created sass/css styling for page
    - Styling for page elements

--- index.php
+++ index.php
@@ -3,14 +3,42 @@
 <html>
     <head>
         <title>Mason Santora Portfolio</title>
-        <link rel='stylesheet' type='text/css' href='style/css/style.css'>
+        <link rel='stylesheet' type='text/css' href='style/css/landing.css'>
     </head>
     
-    <body>
+    <body id='landingPage'>
     
-        <h2>test</h2>
-        <i class="fas fa-user-circle"></i>
+        <!-- Navigation Bar -->
+        <div class='sidenav'>
+            <!-- Upper Navigation -->
+            <div class='upperNav'>
+                <a href='#'><i class="fas fa-home"></i></a>
+                <a href='#'><i class="fas fa-user-circle"></i></a>
+                <a href='#'><i class="fas fa-project-diagram"></i></a>
+            </div>
+            
+            <!-- Lower Navigation -->
+            <div class='lowerNav'>
+                <a href='#'><i class="fas fa-envelope"></i></a>
+                <a href='#'><i class="fab fa-linkedin-in"></i></a>
+                <a href='#'><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+        
+        <!-- Main Content -->
+        <div class='container'>
+            <h1><span>Hi,</span><span>I'm Mason, your</span><span>friendly neighborhood</span><span>programmer</span></h1>
+            <h2>
+                <span class='typer' id='main' data-words='Full Stack Developer, Software Engineer, System Integration Tester, Web Application Developer, Project Manager'  data-colors='#E6E6E6' data-delay="100" data-deleteDelay="1000"></span>
+                <span class='cursor' data-owner='main'></span>
+            </h2>
+            
+            
+            <div class='overlay'><img></div>
+        </div>
+        
         
     </body>
-
+    <!-- Script for auto typer -->
+    <script src='js/typer.js'></script>
 </html>
