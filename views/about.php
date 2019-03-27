0c28a256c4d4832ff22e0f9ce82bf7cd62d04013
Mason Santora <masonsantora@gmail.com>
2019-03-27 03:26:46 -0400

    About Page
    - Added text & links
    - Finished styling for links, and positions of text
    
    Projects page
    - Inital buildout/styling

--- views/about.php
+++ views/about.php
@@ -18,7 +18,9 @@
         <!-- Main Content -->
         <div class='container'>
             <h1>About me</h1>
-            <h2>If you would like to reach out to me for further questions, don't hesitate to reach me through the form below. Or call me at (267) 371 0626</h2>
+            <h2><span>Since graduating with a B.A in Information Technology at Rochester Institute of Technology (RIT), my focus in my professional career is to develop practical and functional applications for any problem.</span>
+                <span>During a paid internship at <a href='https://www.clevermethod.com/' target='_blank'>clevermethod</a>, I had the responsiblity to develop web applications for the millions with the client <a href='https://www.sharkninja.com/' target='_blank'>SharkNinja</a>. The reward of knowing that your creation is being used by millions of people, is a feeling of accomplishment and creates a hunger to develop even larger applications to change the world.</span>
+                <span>When not online, you can find me spending time with my family, practicing my <a href='https://www.flickr.com/photos/137704097@N04/' target='_blank'>photography</a> techniques, or in the online world of Azeroth.</span></h2>
             
             <div class='overlay'><img></div>
         </div>
---
+++ views/projects.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Mason Santora Portfolio</title>
+        <link rel='stylesheet' type='text/css' href='../style/css/projects.css'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="../js/main.js"></script>
+    </head>
+    
+    <body id='projectsPage'>
+    
+        <?php
+            //Load in navigation bar
+            include 'navigation.php';
+        ?>
+        
+        <!-- Main Content -->
+        <div class='container'>
+            <h1>Ninja Kitchen</h1>
+            
+            <p>- Full scale front-end builds of desktop and mobile experiences for<br>predominant pages<br>
+                - System integration tests between staging and production servers<br>
+                - Quality assurance solutions<br>
+                - Data entry of various components<br>
+                - Monthly traffic in the millions<br>
+                - Award winning e-commerce website</p>
+            <div class='overlay'><img></div>
+        </div>
+        
+    </body>
+</html>
